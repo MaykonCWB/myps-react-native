7bf25beb823eeefcfe8691117c69c30d473e8716
Maykon Melo <maykonctba@gmail.com>
2023-09-03 01:18:09 -0300

    second commit imgs

--- App.jsx
+++ App.jsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View >
-      <Text>Lets`s code</Text>
+      <Text>Lets`s codeeee</Text>
       <StatusBar />
     </View>
   );
