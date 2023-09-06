cc0c36211d65350bae2aba65b23a2ec1ea0862fd
Maykon Melo <maykonctba@gmail.com>
2023-09-06 01:00:24 -0300

    Folder structure and initial settings

--- App.jsx
+++ App.jsx
@@ -1,13 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Home} from './src/screens/Home'
 
 export default function App() {
-  return (
-    <View >
-      <Text>Lets`s codeeee</Text>
-      <StatusBar />
-    </View>
-  );
+    return (
+      <Home/>
+    )
 }
-
